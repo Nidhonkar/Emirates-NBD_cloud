5feb99800c24f09b19a5d58979bb6b86ce9c8a70
Nidhonkar <nidhonkardattaprasad@gmail.com>
2025-08-22 10:19:06 +0400

    Delete pages directory

--- pages/2_Timeline.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-import plotly.express as px
-from components.utils import load_csv
-
-st.title("ENBD Digital Transformation Timeline")
-df = load_csv("data/enbd_timeline.csv")
-fig = px.scatter(df, x="Year", y="Type", color="Type", hover_name="Milestone", size=[10]*len(df))
-st.plotly_chart(fig, use_container_width=True)
--- pages/5_Function_Impact.py
+++
@@ -1,7 +0,0 @@
-
-import streamlit as st
-from components.utils import load_csv
-
-st.title("Function-Wise Transformation")
-df = load_csv("data/function_before_after.csv")
-st.dataframe(df, use_container_width=True)
